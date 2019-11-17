a89ca83d32bef23a0312aaea18f6305fd290b4f8
Jeffery <evilmaya2003@gmail.com>
2019-11-17 12:07:41 +0800

    add while true

--- class3.py
+++ class3.py
@@ -34,8 +34,21 @@ else:
 
 # while
 
+"""
 x = 5
 while x < 10:
     print('x小於10喔!')
     x = x + 1
 print('我逃出了!')
+"""
+
+while True:
+    mode = input('請輸入遊戲模式: ')
+    if mode == 'q': #quit
+        break
+    elif mode == '1':
+        print('啟動模式一')
+    elif mode == '2':
+        print('啟動模式二')
+    else:
+        print('只能輸入1/2/q')
