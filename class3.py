f838993c5b8dde359b1effe24783edf2029bb312
Jeffery <evilmaya2003@gmail.com>
2019-11-17 10:47:41 +0800

    class3

---
+++ class3.py
@@ -0,0 +1,16 @@
+# if
+
+"""
+rain = input('請問今天有沒有下雨:')
+
+if rain == '有':
+    print('撐傘出門')
+    print('買一包洋芋片')
+    print('在家看電影')
+"""
+
+# 型別轉換(casting)
+age = input('請輸入年齡:')
+age = int(age)
+if age >= 20:
+    print('你可以投票')
