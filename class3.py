5f1a22ebf206a25aa22410861c678b9689b87f90
Jeffery <evilmaya2003@gmail.com>
2019-11-17 11:26:46 +0800

    add age

--- class3.py
+++ class3.py
@@ -9,8 +9,23 @@ if rain == '有':
     print('在家看電影')
 """
 
-# 型別轉換(casting)
+# 型別轉換(casting),if,elif
+"""
 age = input('請輸入年齡:')
 age = int(age)
 if age >= 20:
     print('你可以投票')
+else:
+    print('你還不能投票')
+"""
+
+age = input('請輸入年齡:')
+age = int(age)
+if age < 13:
+    print('國小')
+elif age >= 13 and age < 18:
+    print('國高中')
+elif age >= 18 and age < 22:
+    print('大學')
+else:
+    print('社會')
