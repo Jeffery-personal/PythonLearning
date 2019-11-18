f8a8aa15da32d5f35665410d7f08e5817e034083
Jeffery <evilmaya2003@gmail.com>
2019-11-18 13:57:00 +0800

    check if file exist

--- products.py
+++ products.py
@@ -1,6 +1,31 @@
-# product如何又裝名字又裝清單 ? 二維清單(小清單放到大清單)
+import os   #operating system
 
 products = []
+if os.path.isfile('products.csv'):  # 檢查檔案是否存在
+    print('找到檔案')
+    # product如何又裝名字又裝清單 ? 二維清單(小清單放到大清單)
+    """
+    讀取檔案
+    1. 增加讀取檔案程式碼
+    2. 用split(',')來用逗點做分割
+    3. 用strip()來除掉換行符號(\n)
+    4. 加上encoding='utf-8'來讀取
+    5. 解釋split完會變清單
+    6. 把讀到的內容裝進清單
+    """
+    with open('products.csv', 'r', encoding = 'UTF-8') as f: # 記得編碼
+        for line in f:
+            # 跳過欄位名稱
+            if '商品,價格' in line:
+                continue
+            # 注意! split切割完的結果是清單
+            name, price = line.strip().split(',')     # 先把換行符號去掉,再逗點分割
+            products.append([name, price])
+    print(products)    
+else:
+    print('找不到檔案')
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -15,6 +40,7 @@ while True:
     # products.append(name)
 print(products)
 
+# 印出商品紀錄
 for p in products:
     print(p)
     print(p[0], '的價格是', p[1])
@@ -43,6 +69,8 @@ Excel導入CSV資料:
 編碼: 選UTF-8
 分格符號: 選逗點
 """
+
+# 寫入檔案
 with open('products.csv','w', encoding='utf-8') as f:   # 加入編碼避免寫入中文時造成亂碼
     #f.write('商品,價格\n')                  # 這樣寫顯示會有問題(編碼問題造成, 上行須加入編碼)
     for p in products:
